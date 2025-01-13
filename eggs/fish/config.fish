91e4e02301843d5a3de00e998c8b100edae2d9ab
BitDragon256 <bergben44@gmail.com>
2025-01-13 21:45:16 +0100

    added laptop

--- eggs/fish/config.fish
+++ eggs/fish/config.fish
@@ -1,9 +1,6 @@
 function fish_greeting
     neofetch
 end
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
 
 alias yay='paru -S'
 alias yays='paru -Ss'
@@ -16,23 +13,24 @@ alias :q='exit'
 alias :q!='systemctl poweroff'
 alias restart='systemctl reboot'
 
-# temporary fix
-alias intellij-fix='/opt/intellij-idea-ultimate-edition/bin/idea.sh'
-
 fish_add_path /home/bitdragon/.spicetify
 
-# colors
-set -U fish_color_autosuggestion red
-set -U fish_color_command	 cyan
-set -U fish_color_comment        yellow
-set -U fish_color_end            cyan
-set -U fish_color_error          blue
-set -U fish_color_escape         cyan
-set -U fish_color_normal         blue
-set -U fish_color_operator       blue
-set -U fish_color_param          yellow
-set -U fish_color_quote          blue
-set -U fish_color_redirection    blue
-set -U fish_color_selection      cyan
+# {% if on_desktop %}
+
+#<yolk> # colors
+#<yolk> set -U fish_color_autosuggestion red
+#<yolk> set -U fish_color_command	     cyan
+#<yolk> set -U fish_color_comment        yellow
+#<yolk> set -U fish_color_end            cyan
+#<yolk> set -U fish_color_error          blue
+#<yolk> set -U fish_color_escape         cyan
+#<yolk> set -U fish_color_normal         blue
+#<yolk> set -U fish_color_operator       blue
+#<yolk> set -U fish_color_param          yellow
+#<yolk> set -U fish_color_quote          blue
+#<yolk> set -U fish_color_redirection    blue
+#<yolk> set -U fish_color_selection      cyan
+
+# {% end %}
 
 starship init fish | source
--- eggs/hypr/autostart.sh
+++ eggs/hypr/autostart.sh
@@ -18,18 +18,23 @@ rfkill unblock all &
 # set the cursor
 # hyprctl setcursor Bibata-Modern-Classic 24 &
 
+# {% if on_laptop %}
 # launch hyprpaper (wallpaper daemon thingy wtv)
 # hyprpaper &
 
+# {% if on_desktop %}
 # launch wallpaper engine
 $config/wallpapers.sh >> $config/log/wallpaper.log &
+# {% end %}
 
 # launch pipewire
 ~/.local/bin/start-pipewire
 
+# {% if on_desktop %}
 # rest of autostart
 signal-desktop &
 discord --start-minimized &
 steam -silent &
 sleep 3
 spotify-tray --hide-window &
+# {% end %}
